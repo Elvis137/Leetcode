0723df36d00ea72c81e90e975dfab58bcfada340
Elvis137 <103258410+Elvis137@users.noreply.github.com>
2025-10-29 10:01:50 +0530

    Time: 0 ms (100%), Space: 12.5 MB (78.76%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-int dp[201][201];
-int recur(vector<vector<int>>&arr ,int i,int j)
-{ long long a=0,b=0;
-if(dp[i][j]!=-1)return dp[i][j];
-    if(i==arr.size()-1)
-    {
-    return dp[i][j]= arr[i][j];
-    }
-
-    a=recur(arr,i+1,j+1)+arr[i][j];
-   b= recur(arr,i+1,j)+arr[i][j];
-    return dp[i][j]=min(a,b);
-
-}
     int minimumTotal(vector<vector<int>>& triangle) {
-      memset(dp,-1,sizeof(dp));
-      return  recur(triangle,0,0);
+        int n=triangle.size();
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=0;j<triangle[i].size();j++)
+            {
+            triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
+            }
+        }
+        return triangle[0][0];
     }
 };
