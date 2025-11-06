96703f1e70e4b7642a885178eb7b45fde688ccb0
Elvis137 <103258410+Elvis137@users.noreply.github.com>
2025-11-06 09:10:31 +0530

    Time: 87 ms (76.97%) | Memory: 13.3 MB (93.66%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-int dp[201][10001];
-bool  recur(vector<int>&nums,int i,int tar)
-{
-    if(i>=nums.size()||tar<0)return false;
-
-    if(tar==0) {
-    dp[i][tar]=true;
-    return  dp[i][tar];
-    }
-    if( dp[i][tar]!=-1)return  dp[i][tar];
-
-    int take=false,ntake=false;
-    if(nums[i]<=tar){
-    take=recur(nums,i+1,tar-nums[i]);
-    
-    }
-        ntake=recur(nums,i+1,tar);
-
-    return dp[i][tar]=take||ntake;
-
-}
     bool canPartition(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        int s=0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++)s=s+nums[i];
-        if(s%2==1){cout<<1;return false;}
-        else return recur(nums,0,s/2);
-        
+        int sum=0;
+   for(int i=0;i<nums.size();i++)
+   {
+    sum+=nums[i];
+   }
+   if(sum%2==1)return false;
+   int tar=sum/2;
+  vector<bool>dp(tar+1,false);
+
+   dp[0]=true;
+   for(auto num:nums)
+   {
+    for(int j=tar;j>=num;j--)
+    {
+        dp[j]=dp[j]||dp[j-num];
+    }
+   }
+        return dp[tar];
     }
 };
