f8d956b94b98cbf15e897895f3c6d90550026f96
Elvis137 <103258410+Elvis137@users.noreply.github.com>
2026-01-06 11:22:40 +0530

    Time: 275 ms (54.54%) | Memory: 10.6 MB (70.12%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
+int recur(vector<vector<char>>&mat,string &word,int i,int j,int k,vector<vector<int>>&v)
+{
+    
+    if(word.length()==k)return 1;
+    if(i>=mat.size()||i<0||j>=mat[0].size()||j<0||word[k]!=mat[i][j])return 0;
+    char temp=mat[i][j];
+    mat[i][j]='#';
+    for(int t=0;t<4;t++)
+    {
+    if (recur(mat, word, i + v[t][0], j + v[t][1], k + 1,v)) {
+            mat[i][j] = temp;   
+            return 1;
+        }    }
+    mat[i][j]=temp;
+    return 0;
+}
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        
-        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
-            if (k == word.length()) {
-                return true;
-            }
-            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
-                return false;
-            }
-            
-            char temp = board[i][j];
-            board[i][j] = '\0';
-            
-            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
-                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
-                return true;
-            }
-            
-            board[i][j] = temp; 
-            return false;
-        };
-        
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
-                    return true;
+        int len=word.length();
+        int n=board.size();
+        int m=board[0].size();
+         vector<vector<int>>v={{1,0},{0,1},{-1,-0},{0,-1}};
+
+        if(len>m*n)return false;
+
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(board[i][j]==word[0])
+                {
+                    if(recur(board,word,i,j,0,v))return true;
                 }
             }
         }
         return false;
+        
     }
 };
