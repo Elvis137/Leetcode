acc2875b2a88bab4186efcb33b9b9b2627557fe7
Elvis137 <103258410+Elvis137@users.noreply.github.com>
2026-01-11 21:13:58 +0530

    Time: 3 ms (28.35%), Space: 10.6 MB (49.11%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,35 @@
 class Solution {
-    void recurse(int ind,vector<int>&ds,vector<vector<int>>&ans)
-    {
-        int i=0;
-        if(ind==ds.size())
-        {
-            ans.push_back(ds);
-            return;
-            
-        }
-        
-        for(i=ind;i<ds.size();i++)
-        {
-            swap(ds[ind],ds[i]);
-              recurse(ind+1,ds,ans);
-              swap(ds[ind],ds[i]);
-
-        }   
-
-         }
 public:
+void recur(vector<int>&nums,vector<int>&v,vector<vector<int>>&ans,vector<int>&a)
+{
+    if(v.size()==nums.size())
+    {
+        ans.push_back(v);
+        return;
+    }
+    for(int i=0;i<nums.size();i++)
+
+    {
+        if(!a[i])
+        {
+            a[i]=1;
+            v.push_back(nums[i]);
+            recur(nums,v,ans,a);
+            a[i]=0;
+            v.pop_back();
+
+        }
+
+    }
+}
+
     vector<vector<int>> permute(vector<int>& nums) {
+
         vector<vector<int>>ans;
-        recurse(0,nums,ans);
+        vector<int>a(nums.size(),0);
+        vector<int>v;
+        recur(nums,v,ans,a);
+        return ans;
         
-     return ans;   
     }
 };
