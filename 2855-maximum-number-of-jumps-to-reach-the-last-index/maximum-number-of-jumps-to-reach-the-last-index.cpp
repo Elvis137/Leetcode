17f8d4d7a2612b2ef11ef4f4be21a5e5fe3bf476
Elvis137 <103258410+Elvis137@users.noreply.github.com>
2026-01-18 10:46:02 +0530

    Time: 21 ms (53.31%) | Memory: 68.4 MB (66.91%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-int recur(vector<int>& nums, int tar,int st, vector<int> &dp)
-{
-    if(st==nums.size()-1)return 0; 
-            if (dp[st] != -1) return dp[st];
-
-        int m=INT_MIN;
-
-    for(int i=st+1;i<nums.size();i++)
-    {
-        if(abs(nums[i]-nums[st])<=tar)
-        {
-            m=max(m,1+recur(nums,tar,i,dp));
-    }
-    }
-        return dp[st] = m;
-
-}
     int maximumJumps(vector<int>& nums, int target) {
-            vector<int> dp(nums.size(), -1);
-     int ans=recur(nums,target,0,dp);
-        if(ans>0)
-           return ans;
-        else
-            return -1;        
+        int n = nums.size();
+        vector<int> dp(n, -1);
+
+        dp[0] = 0;  // starting point
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] != -1 && abs(nums[i] - nums[j]) <= target) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1] == -1 ? -1 : dp[n - 1];
     }
 };
