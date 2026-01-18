d811a1b98d5eea5fe3c8ea8177d34754ca03defa
Elvis137 <103258410+Elvis137@users.noreply.github.com>
2026-01-18 11:07:33 +0530

    Time: 727 ms (5.02%), Space: 21.6 MB (21.34%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+// int recur(vector<int>nums,int i){
+//     if(i>=nums.size())return 0;
+//    for(int i=0;i<nums.size();i++)
+//    {
+//     recur(nums,i+)
+//    }
+// }
+    int jump(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> dp(n, INT_MAX);
+
+        dp[0] = 0;  
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] != INT_MAX&&(j+nums[j]>=i) ) {
+                    dp[i] = min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n - 1] == INT_MAX ? -1 : dp[n - 1];
+    }
+};
